c15becc14414275398f111675ca2000e32e5091d
Benjamn <bfreeman@lcs.on.ca>
2021-01-08 12:25:39 -0500

    Initial Commit

---
+++ NumberSytems.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+var str = "Hello, playground"
+// find the digits that represent 27 in binary
+// we know this is 11011 in Base 2
+
+// use the remandor operator to get the first binary digit
+// the remainder operator is %
+27 % 2
+// find the value left to conver
+// the devision operator is /
+27 / 2
+
+13 % 2
+13 / 2
+
+
